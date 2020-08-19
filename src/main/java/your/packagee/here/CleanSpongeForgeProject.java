61b1b35f36532c3233e036dc1c2563e1d04f1e34
HoverCatz <1442391+HoverCatz@users.noreply.github.com>
2020-08-19 15:16:16 +0200

    Initial commit.

---
+++ src/main/java/your/packagee/here/CleanSpongeForgeProject.java
@@ -0,0 +1,23 @@
+package your.packagee.here;
+
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.game.state.GameStartedServerEvent;
+import org.spongepowered.api.plugin.Dependency;
+import org.spongepowered.api.plugin.Plugin;
+
+@Plugin(
+    id = "your_mod_id_here",
+    name = "your_mod_name_here",
+    dependencies = @Dependency(id = "gooeylibsponge"),
+    authors = ""
+)
+public class CleanSpongeForgeProject
+{
+
+    @Listener
+    public void onServerStart(GameStartedServerEvent event)
+    {
+        System.out.println(":D");
+    }
+
+}
